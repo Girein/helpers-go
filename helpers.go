3a1cfe175d347f88a0d207cceec6eb8aedbe03e8
Girein <giovanni.reinard@gmail.com>
2021-03-19 12:12:38 +0700

    update GormOpen()

--- helpers.go
+++ helpers.go
@@ -22,12 +22,9 @@ func ToDateTimeString(dateTime time.Time) string {
 // GormOpen returns database connection
 func GormOpen(driver string) (*gorm.DB, error) {
 	db, err := gorm.Open(driver, os.Getenv("DB_CONNECTION_URL"))
-	if err != nil {
-		LogIfError(err, "Failed to connect database")
-		return nil, err
-	}
+	LogIfError(err, "Failed to connect database")
 
-	return db, nil
+	return db, err
 }
 
 // LogIfError logs the error with message
